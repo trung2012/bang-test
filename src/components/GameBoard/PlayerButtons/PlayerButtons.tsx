5bfff13a058cd1910e5371f2c3439d64065257c9
Trung Pham <trungpham2012@gmail.com>
2020-11-27 22:07:53 -0800

    fix player1 layout css

--- src/components/GameBoard/PlayerButtons/PlayerButtons.tsx
+++ src/components/GameBoard/PlayerButtons/PlayerButtons.tsx
@@ -8,7 +8,7 @@ import './PlayerButtons.scss';
 import { delayBetweenActions, IGamePlayer, stageNames } from '../../../game';
 
 export const PlayerButtons: React.FC<{ player: IGamePlayer }> = ({ player }) => {
-  const { G, moves, playerID } = useGameContext();
+  const { G, moves, playerID, isActive } = useGameContext();
   const { setError } = useErrorContext();
   const isCurrentPlayer = playerID === player.id;
   const isReactingToBullets =
@@ -66,7 +66,7 @@ export const PlayerButtons: React.FC<{ player: IGamePlayer }> = ({ player }) => 
               <PowerIcon className='player-button-icon' />
             </PlayerButton>
           )}
-          {player.id === playerID && (
+          {player.id === playerID && isActive && (
             <PlayerButton tooltipTitle='End turn' onClick={onPassClick}>
               <PassIcon className='player-button-icon' />
             </PlayerButton>
